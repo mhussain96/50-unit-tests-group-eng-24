215a9877884f5fc23371cbb080bf11a52085a660
Michael Wright <mwright@spartaglobal.com>
2019-02-11 22:01:49 +0000

    Added Sorted list test

--- Group1/Collection workspace/Collection workspace/Class1.cs
+++ Group1/Collection workspace/Collection workspace/Class1.cs
@@ -1,127 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace Collection_workspace
 {
-
-    //Amira's Solutions
-    public class CollectionsTesting
+    
+    public class myList1
     {
-        static Dictionary<string, string> dict = new Dictionary<string, string>();
-        //Create Dictionary additions of books with authors as their key (two strings). 
-
-        public Dictionary<string, string> DictionaryTest1()
+    static int[,] mySquareArray = new int[10, 10];
+        public int IntegerList(int size)
         {
-            return dict;
-        }
-    }
-    //Maiwand's Solutions
-    public class Class1
-    {
-        public string StackMethod()
-        {
-            Stack<string> myStack = new Stack<string>();
-            myStack.Push("Hello");
-            myStack.Push("World");
-            myStack.Push("!");
-
-
-            Console.WriteLine(myStack);
-            // what does this stack return
-
-            return "Something";
-        }
-
-        public int AnotherStack()
-        {
-            Stack<int> newStack = new Stack<int>();
-            newStack.Push(1);
-            newStack.Push(2);
-            newStack.Push(3);
-            newStack.Push(4);
-            newStack.Push(5);
-            newStack.Push(6);
-
-            Console.WriteLine(newStack.Pop());
-            Console.WriteLine(newStack.Pop());
-            // how many does this stack return
-
+            //Create a square array with all the values from 0 - 99
+            //Add the values from the array to a list in numerical order
+            //Return the number in the 44th position
+            //Should return 44
             return -1;
         }
-        
-        public int AnotherStack2()
+    }
+    public class myList2
+    {
+        public List<string> ClassList()
         {
-            Stack<int> AnotherStack2 = new Stack<int>();
-            for (int i = 1; i < 51; i++)
-            {
-                AnotherStack2.Push(i);
-            }
-
-            return AnotherStack2.Peek();
-        }
-
-        public int AnotherStack3()
-        {
-            Stack<int> AnotherStack3 = new Stack<int>();
-            for (int i = 1; i < 51; i++)
-            {
-                // add 10 to the stack without changing length
-            }
-
-            return AnotherStack3.Peek();
+            List<string> listOfClassMembers = new List<string>();
+            return listOfClassMembers;
+            //Delete the two lines above
+            //Create a list of class members
+            //return the list
         }
     }
-          
-        //Michael's Solutions
-          public class myList1
-          {
-          static int[,] mySquareArray = new int[10, 10];
-              public int IntegerList(int size)
-              {
-                  //Create a square array with all the values from 0 - 99
-                  //Add the values from the array to a list in numerical order
-                  //Return the number in the 44th position
-                  //Should return 44
-                  return -1;
-              }
-          }
-          
-          public class myList2
-          {
-              public List<string> ClassList()
-              {
-                  List<string> listOfClassMembers = new List<string>();
-                  return listOfClassMembers;
-                  //Delete the two lines above
-                  //Create a list of class members
-                  //return the list
-              }
-          }
-          
-          public class myList3
-          {
-              //Create a constructor for creating a book
-              //Include its name, author and release year
-              //Create a list of 4 books as objects
-              //And return the list
-              public class Book
-              {
-                  public Book()
-                  {
+    public class myList3
+    {
+        //Create a constructor for creating a book
+        //Include its name, author and release year
+        //Create a list of 4 books as objects
+        //And return the list
+        public class Book
+        {
+            public Book()
+            {
 
-                  }
-              }
+            }
+        }
+        public List<Book> Library()
+        { 
+            var booklist = new List<Book>();
+            return booklist;
+        }
+    }
+    public class myList4
+    {
+        public Dictionary<int, string> topFilms = new Dictionary<int, string>();
+        public Dictionary<int, string> TopFilms()
+        {
+            //Put all these films in a list in order of release date
+            topFilms.Add(1994, "Shawshank Redemption");
+            topFilms.Add(1972, "The Godfather");
+            topFilms.Add(2008, "The Dark Knight");
+            topFilms.Add(1957, "12 Angry Men");
+            topFilms.Add(1993, "Schindler's List");
+            topFilms.Add(1994, "Pulp Fiction");
+            topFilms.Add(1966, "The Good, the Bad and the Ugly");
+            topFilms.Add(1999, "Fight Club");
+            topFilms.Add(2010, "Inception");
+            topFilms.Add(2018, "Into the Spider-Verse");
 
-              public List<Book> Library()
-              { 
-                  var booklist = new List<Book>();
-                  return booklist;
-
-              }
-          }
+        return topFilms;
+        }
     }
 }
