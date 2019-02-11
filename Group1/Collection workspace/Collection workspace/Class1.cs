1bc2f450ba6b5a293423809010a0fb8aea67f1c5
Maiwand Hussain <mhussain2@spartaglobal.com>
2019-02-11 17:35:06 +0000

    mage

--- Group1/Collection workspace/Collection workspace/Class1.cs
+++ Group1/Collection workspace/Collection workspace/Class1.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,48 +9,49 @@ namespace Collection_workspace
     
     public class Class1
     {
-      
-    }   
-    public class myList1
-    {
-    static int[,] mySquareArray = new int[10, 10];
-        public int IntegerList(int size)
+        public string StackMethod()
         {
-            //Create a square array with all the values from 0 - 99
-            //Add the values from the array to a list in numerical order
-            //Return the number in the 44th position
-            //Should return 44
+            Stack<string> myStack = new Stack<string>();
+            myStack.Push("Hello");
+            myStack.Push("World");
+            myStack.Push("!");
+
+
+            Console.WriteLine(myStack);
+            // what does this stack return
+
+            return "Something";
+        }
+
+        public int AnotherStack()
+        {
+            Stack<int> newStack = new Stack<int>();
+            newStack.Push(1);
+            newStack.Push(2);
+            newStack.Push(3);
+            newStack.Push(4);
+            newStack.Push(5);
+            newStack.Push(6);
+
+            Console.WriteLine(newStack.Pop());
+            Console.WriteLine(newStack.Pop());
+
+            // how many does this stack return
+
             return -1;
         }
-    }
-    public class myList2
-    {
-        public List<string> ClassList()
+        
+        public int AnotherStack2()
         {
-            List<string> listOfClassMembers = new List<string>();
-            return listOfClassMembers;
-            //Delete the two lines above
-            //Create a list of class members
-            //return the list
-        }
-    }
-    public class myList3
-    {
-        //Create a constructor for creating a book
-        //Include its name, author and release year
-        //Create a list of 4 books as objects
-        //And return the list
-        public class Book
-        {
-            public Book()
+            Stack<int> AnotherStack2 = new Stack<int>();
+            for (int i = 1; i < 51; i++)
             {
-
+                AnotherStack2.Push(i);
             }
-        }
-        public List<Book> Library()
-        { 
-            var booklist = new List<Book>();
-            return booklist;
+
+            // remove 10 from the top of stack
+
+            return AnotherStack2.Peek();
         }
     }
 }
