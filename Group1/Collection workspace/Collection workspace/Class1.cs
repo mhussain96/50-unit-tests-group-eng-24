cfbba1ac2bd0ca11d97bda5ed589351c89f05d9b
Maiwand Hussain <mhussain2@spartaglobal.com>
2019-02-11 17:18:14 +0000

    Stack test

--- Group1/Collection workspace/Collection workspace/Class1.cs
+++ Group1/Collection workspace/Collection workspace/Class1.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace Collection_workspace
 {
+    
     public class Class1
     {
+        public string StackMethod()
+        {
+            Stack<string> myStack = new Stack<string>();
+            myStack.Push("Hello");
+            myStack.Push("World");
+            myStack.Push("!");
+
+
+            Console.WriteLine(myStack);
+            return "Something";
+        }
+
+        public int AnotherStack()
+        {
+            Stack<int> newStack = new Stack<int>();
+            newStack.Push(1);
+            newStack.Push(2);
+            newStack.Push(3);
+            newStack.Push(4);
+            newStack.Push(5);
+            newStack.Push(6);
+
+            Console.WriteLine(newStack.Pop());
+            Console.WriteLine(newStack.Pop());
+            return -1;
+        }
+        
+        public int AnotherStack2()
+        {
+            Stack<int> AnotherStack2 = new Stack<int>();
+            for (int i = 1; i < 51; i++)
+            {
+                AnotherStack2.Push(i);
+            }
+
+            return AnotherStack2.Peek();
+        }
+
+        //static Stack<int> AnotherStack2 = new Stack<int>();
+
+        //public Stack<int> TestMethod3()
+        //{
+
+        //    for (int i = 0; i < 50; i++)
+        //    {
+
+        //    }
+
+        //    return AnotherStack2;
+        //}
     }
 }
--- Group1/Collection workspace/Collections test/UnitTest1.cs
+++ Group1/Collection workspace/Collections test/UnitTest1.cs
@@ -9,6 +9,48 @@ namespace Collections_test
         [TestMethod]
         public void TestMethod1()
         {
+            // arrange
+            var instance1 = new Collection_workspace.Class1();
+            var expected = "Hello";
+            // act 
+            var actual = instance1.StackMethod();
+            // assert 
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // arrange
+            var instance2 = new Collection_workspace.Class1();
+            var expected = 5;
+            // act 
+            var actual = instance2.AnotherStack();
+            // assert 
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            // arrange
+            var instance3 = new Collection_workspace.Class1();
+            var expected = 40;
+            // act 
+            var actual = instance3.AnotherStack2();
+            // assert 
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        //var instance3 = new Collection_workspace.Class1();
+        //var expected = instance3.AnotherStack2();
+        //var expectedCount = 40;
+        //// act 
+        //var actual = instance3.TestMethod3();
+        //var actualCount = actual.Count;
+        //// assert 
+        //Assert.AreEqual(expectedCount, actualCount);
+        //    Assert.IsInstanceOfType(actual, expected.GetType());
     }
 }
