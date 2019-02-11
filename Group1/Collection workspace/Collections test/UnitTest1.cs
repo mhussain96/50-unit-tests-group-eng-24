9b3137bb38cdc85adfb5980fad9974e6540e1012
Michael Wright <mwright@spartaglobal.com>
2019-02-11 17:03:55 +0000

    Added a test for string lists, and object lists

--- Group1/Collection workspace/Collections test/UnitTest1.cs
+++ Group1/Collection workspace/Collections test/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Collection_workspace;
+using System.Collections.Generic;
 
 namespace Collections_test
 {
@@ -13,11 +14,40 @@ namespace Collections_test
             //Arrange
             var instance01 = new Collection_workspace.myList1();
             int expectedValue = 44;
-
             //Act
-            var actual = instance01.IntegerList();
+            var actual = instance01.IntegerList(10);
             //Assert
             Assert.AreEqual(expectedValue, actual);
+        }
+        [TestMethod]
+        public void List2_Test()
+        {
+            //Arrange
+            var instance02 = new Collection_workspace.myList2();
+            var expected = new List<string>();
+            var expectedCount = 12;
+
+            //Act
+            var actual = instance02.ClassList();
+            var actualCount = actual.Count;
+
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+            Assert.IsInstanceOfType(actual, expected.GetType());
+        }
+        [TestMethod]
+        public void List3_Test()
+        {
+            //Arrange
+            var instance03 = new Collection_workspace.myList3();
+            var expected = new List<myList3.Book>();
+            var expectedCount = 4;
+            //Act
+            var actual = instance03.Library();
+            var actualCount = actual.Count;
+            //Assert
+            Assert.AreEqual(expectedCount, actualCount);
+            Assert.IsInstanceOfType(actual, expected.GetType());
         }
     }
 }
