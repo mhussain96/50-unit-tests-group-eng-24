5eef791a866b336a94958381de5094e852f56484
Nadi Adem <NAdem@spartaglobal.com>
2019-02-11 20:37:13 +0000

    first Commit

---
+++ Group3/Group3-tests/UnitTest1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab_01;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var arrayInstance = new lab_01.Class1();
+            var actual = arrayInstance.summation(8.2);
+            var expected = 585.0;
+            Assert.AreEqual(expected,actual);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var arrayInstance2 = new lab_01.Class1();
+            Assert.IsTrue(arrayInstance2.isWithdraw(20));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var arrayInstance3 = new lab_01.Class1();
+            Assert.IsTrue(arrayInstance3.isAllowedEntry(15));
+        }
+    }
+}
---
+++ Group3/Group3-tests/lab-01/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_01
+{
+    public class Class1
+    {
+        // CHALLENGE ONE  
+        // return sum of seq and b , the value of sum should equate to 45 and the total including b =  585
+        
+        public double summation(double b)
+        {
+            double sum = 0;
+            double[] seq = { 20.3, 2.1,b, 14.4 };
+            for (int i = 0; i < seq.Length;i++)
+            {
+                if (seq[2] == 8.2)
+                { 
+                    sum = seq[i] + 0+1+2+3;
+                }
+            }
+            return sum * (double)13;
+        }
+        // Challenge Two
+        public bool isWithdraw(int value)
+        {
+            bool isValuePositive = false;
+            if (value > 10)
+            {
+                isValuePositive = true;
+            }
+            return isValuePositive;
+        }
+        // Challenge Three
+        public bool isAllowedEntry(int value)
+        {
+            bool isAllowed = false;
+            List<int> ages = new List<int>();
+            ages.Add(1);
+            ages.Add(13);
+            ages.AddRange(ages);
+            foreach (var i in ages)
+            {
+                if (value < i)
+                {
+                    isAllowed = true;
+                }
+            }
+           
+            return isAllowed;
+        }
+
+
+    }
+}
